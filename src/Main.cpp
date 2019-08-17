2e9bbf364672547e9789facb8ea004c7537dd9b5
barsandcat <barsandcat@Merinda.Merinda>
2019-08-17 16:22:32 +0300

    Use readline example

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,27 @@
 #include <SpaceTycoonModel.h>
 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <readline/readline.h>
+#include <readline/history.h>
+
 int main(int argc, char** argv)
 {
 	Do();
+
+	char* buf;
+	while ((buf = readline(">> ")) != nullptr) 
+	{
+		if (strlen(buf) > 0)
+		{
+			add_history(buf);
+			printf("executing %s\n", buf);
+		}
+
+		free(buf);
+	}
+
 	return 0;
 }
