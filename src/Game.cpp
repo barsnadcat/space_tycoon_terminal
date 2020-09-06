2af01a5e0bf8d6d3476c7889ce13d15a306c0641
Dmitriy Tsahilov <Dmitriy.Ts@fraglab.local>
2020-09-06 14:45:04 +0300

    Add person

--- src/Game.cpp
+++ src/Game.cpp
@@ -3,26 +3,27 @@
 
 Game::Game()
 {
-    mSettlement.reset(new Settlement());    
+    Reset();
 }
 
 void Game::Run(int n)
-{    
-    for (int i = 0; i < n; i++)
-    {
-        mUpdateContext.mCurrentTime++;
-        mSettlement->Update(mUpdateContext);
-        Render();
-    }
+{
+	for (int i = 0; i < n; i++)
+	{
+		mUpdateContext.mCurrentTime++;
+		mSettlement->Update(mUpdateContext);
+		Render();
+	}
 }
 
 void Game::Render() const
 {
-    LOG(INFO) << mUpdateContext.mCurrentTime << " F:" << mSettlement->GetFoods().size() << " P:" << mSettlement->GetPeople().size();
+	LOG(INFO) << mUpdateContext.mCurrentTime << " F:" << mSettlement->GetFoods().size() << " P:" << mSettlement->GetPeople().size();
 }
 
 void Game::Reset()
 {
-    mSettlement.reset(new Settlement());
-    LOG(INFO) << "Reset";
+	mSettlement.reset(new Settlement());
+    mSettlement->AddPerson(std::make_unique<Person>(30000, 10));
+	LOG(INFO) << "Reset";
 }
